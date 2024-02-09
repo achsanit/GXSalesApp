b6d8272dd7df79ced9477aeb5be746b1154e5ba8
Achsani Taqwim <achsanit87@gmail.com>
2024-02-09 11:03:24 +0800

    improve handling ended session

---
+++ app/src/main/java/com/achsanit/gxsales/ui/dialog/SessionEndedDialogFragment.kt
@@ -0,0 +1,69 @@
+package com.achsanit.gxsales.ui.dialog
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
+import androidx.lifecycle.lifecycleScope
+import com.achsanit.gxsales.R
+import com.achsanit.gxsales.data.local.DataStorePreference
+import com.achsanit.gxsales.data.local.SharedPreferencesManager
+import com.achsanit.gxsales.databinding.FragmentSessionEndedDialogBinding
+import kotlinx.coroutines.launch
+import org.koin.android.ext.android.inject
+
+class SessionEndedDialogFragment(
+    private val navigation: () -> Unit
+) : DialogFragment() {
+
+    private var _binding: FragmentSessionEndedDialogBinding? = null
+    private val binding get() = _binding!!
+
+    private val dataPref by inject<DataStorePreference>()
+    private val sharedPref by inject<SharedPreferencesManager>()
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        dialog?.window?.setBackgroundDrawableResource(R.drawable.bg_round_corner_dialog)
+        _binding = FragmentSessionEndedDialogBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        with(binding) {
+            btnOkay.setOnClickListener {
+                dismiss()
+                lifecycleScope.launch {
+                    dataPref.setLogoutData()
+                    sharedPref.saveData(SharedPreferencesManager.TOKEN_KEY, "")
+                }
+                navigation.invoke()
+            }
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        val width = (resources.displayMetrics.widthPixels * 0.75).toInt()
+        dialog!!.window?.setLayout(width, ViewGroup.LayoutParams.WRAP_CONTENT)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        this.isCancelable = false
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    companion object {
+        const val TAG = "session.ended.dialog"
+    }
+}
--- app/src/main/java/com/achsanit/gxsales/utils/ExtFunction.kt
+++ app/src/main/java/com/achsanit/gxsales/utils/ExtFunction.kt
@@ -1,11 +1,15 @@
 package com.achsanit.gxsales.utils
 
+import android.content.Intent
 import android.util.Patterns
 import android.view.View
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.achsanit.gxsales.R
+import com.achsanit.gxsales.ui.dialog.SessionEndedDialogFragment
+import com.achsanit.gxsales.ui.features.login.LoginActivity
 import com.google.android.material.snackbar.Snackbar
+import java.net.HttpURLConnection
 
 fun CharSequence?.isValidEmail(): Boolean {
     return !isNullOrBlank() && Patterns.EMAIL_ADDRESS.matcher(this).matches()
@@ -25,18 +29,35 @@ fun Fragment.onNetworkEvent(code: Int, messageError: String? = null) {
         else -> { "Something went wrong..." }
     }
 
-    Snackbar.make(
-        requireView(),
-        message,
-        Snackbar.LENGTH_LONG
-    ).setAction(getString(R.string.ok)) {}
-        .setActionTextColor(
-            ContextCompat.getColor(
-                requireContext(),
-                R.color.white
-            )
-        )
-        .setTextColor(ContextCompat.getColor(requireContext(), R.color.white))
-        .setBackgroundTint(ContextCompat.getColor(requireContext(), R.color.red))
-        .show()
+    when(code) {
+        HttpURLConnection.HTTP_UNAUTHORIZED -> {
+            SessionEndedDialogFragment {
+                context?.let {
+                    val intent = Intent(it, LoginActivity::class.java)
+                    it.startActivity(intent)
+                    activity?.finish()
+                }
+            }
+                .show(
+                    parentFragmentManager,
+                    SessionEndedDialogFragment.TAG
+                )
+        }
+        else -> {
+            Snackbar.make(
+                requireView(),
+                message,
+                Snackbar.LENGTH_LONG
+            ).setAction(getString(R.string.ok)) {}
+                .setActionTextColor(
+                    ContextCompat.getColor(
+                        requireContext(),
+                        R.color.white
+                    )
+                )
+                .setTextColor(ContextCompat.getColor(requireContext(), R.color.white))
+                .setBackgroundTint(ContextCompat.getColor(requireContext(), R.color.red))
+                .show()
+        }
+    }
 }
